4a02c87c68afad3a9d6843ce9c03bf4bcc13ab4f
Leonardo <desu987@hotmail.com>
2022-03-07 14:11:52 -0300

    Feat(controllers): added get all vehicles route

--- server/node/src/modules/vehicles/infra/http/controllers/VehiclesController.ts
+++ server/node/src/modules/vehicles/infra/http/controllers/VehiclesController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import Http from '../../../../../shared/error/Http';
 import CreateVehicleService from '../../../services/CreateVehicleService';
+import GetAllVehicleService from '../../../services/GetAllService';
 
 export default class VehiclesController {
   public async create(request: Request, response: Response): Promise<Response> {
@@ -14,6 +15,12 @@ export default class VehiclesController {
 
     response.statusCode = http.response(vehicle);
 
+    return response.json(vehicle);
+  }
+
+  public async getAll(response: Response): Promise<Response> {
+    const getVehicleService = new GetAllVehicleService();
+    const vehicle = await getVehicleService.execute()
     return response.json(vehicle);
   }
 }
--- server/node/src/modules/vehicles/infra/http/routes/vehicles.routes.ts
+++ server/node/src/modules/vehicles/infra/http/routes/vehicles.routes.ts
@@ -4,6 +4,7 @@ import VehiclesController from "../controllers/VehiclesController";
 const vehiclesRouter = Router();
 const vehiclesController = new VehiclesController();
 
+vehiclesRouter.get('/', vehiclesController.getAll);
 vehiclesRouter.post('/', vehiclesController.create);
 
 export default vehiclesRouter;
