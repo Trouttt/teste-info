dd687c880b1e499eea4e61777c2a2230ec3de860
Leonardo <desu987@hotmail.com>
2022-03-07 17:12:11 -0300

    Added(tests): added vehicle delete service test

---
+++ server/node/src/modules/vehicles/services/DeleteVehicleService.spec.ts
@@ -0,0 +1,57 @@
+import assert from 'assert';
+
+import DeleteVehicleService from './DeleteVehicleService';
+
+const makeSut = () => {
+
+  return new DeleteVehicleService();
+
+};
+
+describe('vehicle delete service', () => {
+
+  it('Should return 400 and message if no id is provided', async () => {
+
+    const sut = makeSut();
+
+    const httpRequest = {
+
+      id: '',
+
+    };
+
+    const httpResponse = await sut.execute(httpRequest);
+
+    assert.deepEqual(httpResponse, {
+
+      message: 'Id inválido!!!',
+
+      statusCode: 400,
+
+    });
+
+  });
+
+  it('Should return 400 and message if no vehicle is founded', async () => {
+
+    const sut = makeSut();
+
+    const httpRequest = {
+
+      id: '378866df-27e0-4b13-abd2-fa0a3967aa6e',
+
+    };
+
+    const httpResponse = await sut.execute(httpRequest);
+
+    assert.deepEqual(httpResponse, {
+
+      message: 'O veículo não existe!!!',
+
+      statusCode: 400,
+
+    });
+
+  });
+
+});
